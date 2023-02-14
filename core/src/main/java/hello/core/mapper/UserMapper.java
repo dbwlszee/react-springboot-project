e291b155d8973e6a34a10fffeedfdf6060454892
onozzzs <dnwlals00@gmail.com>
2023-02-14 21:28:06 +0900

    test

---
+++ core/src/main/java/hello/core/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package hello.core.mapper;
+
+import hello.core.vo.UserVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    List<UserVO> userList();
+    UserVO fetchUserByID(int id);
+    void updateUser(UserVO user);
+    void insertUser(UserVO user);
+    void deleteUser(int id);
+
+}
