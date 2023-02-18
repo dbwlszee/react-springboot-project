98971cbb68ec67779e17a0dbf5c2ee893c2cd1bf
Eugene <dbwlszee@gmail.com>
2023-02-18 22:37:47 +0900

    App.js에 UserListComponent컴포넌트 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import UserListComponent from './component/user/UserListComponent';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <UserListComponent/>
     </div>
   );
 }
