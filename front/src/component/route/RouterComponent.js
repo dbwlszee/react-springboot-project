43e1117f8a92af96bde5d83857076b762cc25224
Eugene <dbwlszee@gmail.com>
2023-02-19 19:57:04 +0900

    fix: Route의 element 속성 오류 수정
    element에는 <Component/>의 형식을 써주어야 한다.
    이에 따라 Component를 <Component/>와 같이 바꿔주었다.

--- front/src/component/route/RouterComponent.js
+++ front/src/component/route/RouterComponent.js
@@ -10,10 +10,10 @@ const AppRouter = () => {
             <BrowserRouter>
             <div style={style}>
                 <Routes>
-                    <Route exact path="/" element={UserListComponent}/>
-                    <Route path="/users" element={UserListComponent}/>
-                    <Route path="/add-users" element={AddUserComponent}/>
-                    <Route path="/edit-users" element={EditUserComponent}/>
+                    <Route exact path="/" element={<UserListComponent/>}/>
+                    <Route path="/users" element={<UserListComponent/>}/>
+                    <Route path="/add-users" element={<AddUserComponent/>}/>
+                    <Route path="/edit-users" element={<EditUserComponent/>}/>
                 </Routes>
             </div>
             </BrowserRouter>
