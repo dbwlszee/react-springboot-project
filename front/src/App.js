11ee18a640dc21559c8722977f806bb59fd1d6ac
Eugene <dbwlszee@gmail.com>
2023-03-02 17:13:53 +0900

    feat: navbar 추가
    navbar추가 및 스타일 정의

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AppRouter from './component/route/RouterComponent';
+import UserNav from './component/user/UserNav';
 
 function App() {
   return (
     <div className="App">
+      <UserNav/>
       <AppRouter/>
     </div>
   );
---
+++ front/src/component/user/UserNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './UserNav.module.css';
+
+const UserNav = () => {
+
+    return(
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.home}>
+                    <a href="/">CRUD</a>
+                </div>
+
+                <ul className={styles.menu}>
+                    <li><a href="/add-users">AddUser</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default UserNav;
